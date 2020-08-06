043dc233e8ce5373c6c8220500370c44482c32a8
Aditya <35200774+adit22@users.noreply.github.com>
2020-08-07 05:00:42 +0530

    Add files via upload

--- Restaurant_Server/app.js
+++ Restaurant_Server/app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 var dishrouter = require('./routes/dishrouter');
 var leaderrouter = require('./routes/leaderrouter');
 var promorouter = require('./routes/promorouter');
+const mongoose=require('mongoose');
+const dishes=require('./models/dishes');
+const url='mongodb://localhost:27017/restaurant';
+const connect= mongoose.connect(url);
+connect.then((db)=>{
+console.log("Connected to server");
+},(err)=>{
+  console.log(err);
+});
 
 var app = express();
 
@@ -19,7 +28,56 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
+function auth(req,res,next){
+console.log(req.signedCookies);
+
+if(!req.signedCookies.user){
+
+
+
+var authHeader=req.headers.authorization;
+
+if(!authHeader){
+  var err=new Error('You are not authenticated');
+  res.setHeader('WWW-Authenitcate','Basic');
+  err.status=401;
+  return next(err);
+}
+var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+var username=auth[0];
+var password=auth[1];
+
+if(username==='admin' && password==='password'){
+  res.cookie('user','admin',{signed:true});
+  next();
+
+}
+else{
+  var err=new Error('You are not authenticated');
+  res.setHeader('WWW-Authenitcate','Basic');
+  err.status=401;
+  return next(err);
+}
+}
+
+else{
+  if(req.signedCookies.user==='admin'){
+    next();
+  }
+  else{
+    var err=new Error('You are not authenticated');
+  res.setHeader('WWW-Authenitcate','Basic');
+  err.status=401;
+  return next(err);
+  }
+}
+
+}
+
+app.use(auth);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
